578fda9b233681b47c424c05979eb300fdde5c15
Benjo <ben.ohman@gokstadakademiet.no>
2022-09-29 08:29:34 +0200

    ReactComponent and svg-images

---
+++ src/card-deck.jsx
@@ -0,0 +1,114 @@
+import { ReactComponent as Hearts2 } from "./card-images/cards/2_of_hearts.svg";
+import { ReactComponent as Hearts3 } from "./card-images/cards/3_of_hearts.svg";
+import { ReactComponent as Hearts4 } from "./card-images/cards/4_of_hearts.svg";
+import { ReactComponent as Hearts5 } from "./card-images/cards/5_of_hearts.svg";
+import { ReactComponent as Hearts6 } from "./card-images/cards/6_of_hearts.svg";
+import { ReactComponent as Hearts7 } from "./card-images/cards/7_of_hearts.svg";
+import { ReactComponent as Hearts8 } from "./card-images/cards/8_of_hearts.svg";
+import { ReactComponent as Hearts9 } from "./card-images/cards/9_of_hearts.svg";
+import { ReactComponent as Hearts10 } from "./card-images/cards/10_of_hearts.svg";
+import { ReactComponent as HeartsJ} from "./card-images/cards/jack_of_hearts.svg";
+import { ReactComponent as HeartsQ } from "./card-images/cards/queen_of_hearts.svg";
+import { ReactComponent as HeartsK} from "./card-images/cards/king_of_hearts.svg";
+import { ReactComponent as HeartsAce } from "./card-images/cards/ace_of_hearts.svg";
+
+import { ReactComponent as Spades2 } from "./card-images/cards/2_of_spades.svg";
+import { ReactComponent as Spades3 } from "./card-images/cards/3_of_spades.svg";
+import { ReactComponent as Spades4 } from "./card-images/cards/4_of_spades.svg";
+import { ReactComponent as Spades5 } from "./card-images/cards/5_of_spades.svg";
+import { ReactComponent as Spades6 } from "./card-images/cards/6_of_spades.svg";
+import { ReactComponent as Spades7 } from "./card-images/cards/7_of_spades.svg";
+import { ReactComponent as Spades8 } from "./card-images/cards/8_of_spades.svg";
+import { ReactComponent as Spades9 } from "./card-images/cards/9_of_spades.svg";
+import { ReactComponent as Spades10 } from "./card-images/cards/10_of_spades.svg";
+import { ReactComponent as SpadesJ } from "./card-images/cards/jack_of_spades.svg";
+import { ReactComponent as SpadesQ } from "./card-images/cards/queen_of_spades.svg";
+import { ReactComponent as SpadesK } from "./card-images/cards/king_of_spades.svg";
+import { ReactComponent as SpadesAce } from "./card-images/cards/ace_of_spades.svg";
+
+import { ReactComponent as Diamonds2 } from "./card-images/cards/2_of_diamonds.svg";
+import { ReactComponent as Diamonds3 } from "./card-images/cards/3_of_diamonds.svg";
+import { ReactComponent as Diamonds4 } from "./card-images/cards/4_of_diamonds.svg";
+import { ReactComponent as Diamonds5 } from "./card-images/cards/5_of_diamonds.svg";
+import { ReactComponent as Diamonds6 } from "./card-images/cards/6_of_diamonds.svg";
+import { ReactComponent as Diamonds7 } from "./card-images/cards/7_of_diamonds.svg";
+import { ReactComponent as Diamonds8 } from "./card-images/cards/8_of_diamonds.svg";
+import { ReactComponent as Diamonds9 } from "./card-images/cards/9_of_diamonds.svg";
+import { ReactComponent as Diamonds10 } from "./card-images/cards/10_of_diamonds.svg";
+import { ReactComponent as DiamondsJ } from "./card-images/cards/jack_of_diamonds.svg";
+import { ReactComponent as DiamondsQ } from "./card-images/cards/queen_of_diamonds.svg";
+import { ReactComponent as DiamondsK } from "./card-images/cards/king_of_diamonds.svg";
+import { ReactComponent as DiamondsAce } from "./card-images/cards/ace_of_diamonds.svg";
+
+import { ReactComponent as Clubs2 } from "./card-images/cards/2_of_clubs.svg";
+import { ReactComponent as Clubs3 } from "./card-images/cards/3_of_clubs.svg";
+import { ReactComponent as Clubs4 } from "./card-images/cards/4_of_clubs.svg";
+import { ReactComponent as Clubs5 } from "./card-images/cards/5_of_clubs.svg";
+import { ReactComponent as Clubs6 } from "./card-images/cards/6_of_clubs.svg";
+import { ReactComponent as Clubs7 } from "./card-images/cards/7_of_clubs.svg";
+import { ReactComponent as Clubs8 } from "./card-images/cards/8_of_clubs.svg";
+import { ReactComponent as Clubs9 } from "./card-images/cards/9_of_clubs.svg";
+import { ReactComponent as Clubs10 } from "./card-images/cards/10_of_clubs.svg";
+import { ReactComponent as ClubsJ } from "./card-images/cards/jack_of_clubs.svg";
+import { ReactComponent as ClubsQ } from "./card-images/cards/queen_of_clubs.svg";
+import { ReactComponent as ClubsK } from "./card-images/cards/king_of_clubs.svg";
+import { ReactComponent as ClubsAce } from "./card-images/cards/ace_of_clubs.svg";
+
+export const cardDeck = [ 
+  { suit: "hearts", rank: 11, img: <HeartsAce /> },
+  { suit: "hearts", rank: 2, img: <Hearts2 /> },
+  { suit: "hearts", rank: 3, img: <Hearts3 /> },
+  { suit: "hearts", rank: 4, img: <Hearts4 /> },
+  { suit: "hearts", rank: 5, img: <Hearts5 /> },
+  { suit: "hearts", rank: 6, img: <Hearts6 /> },
+  { suit: "hearts", rank: 7, img: <Hearts7 /> },
+  { suit: "hearts", rank: 8, img: <Hearts8 /> },
+  { suit: "hearts", rank: 9, img: <Hearts9 /> },
+  { suit: "hearts", rank: 10, img: <Hearts10 /> },
+  { suit: "hearts", rank: 10, img: <HeartsJ /> },
+  { suit: "hearts", rank: 10, img: <HeartsQ /> },
+  { suit: "hearts", rank: 10, img: <HeartsK /> },
+
+  { suit: "spades", rank: 11, img: <SpadesAce /> },
+  { suit: "spades", rank: 2, img: <Spades2 /> },
+  { suit: "spades", rank: 3, img: <Spades3 /> },
+  { suit: "spades", rank: 4, img: <Spades4 /> },
+  { suit: "spades", rank: 5, img: <Spades5 /> },
+  { suit: "spades", rank: 6, img: <Spades6 /> },
+  { suit: "spades", rank: 7, img: <Spades7 /> },
+  { suit: "spades", rank: 8, img: <Spades8 /> },
+  { suit: "spades", rank: 9, img: <Spades9 /> },
+  { suit: "spades", rank: 10, img: <Spades10 /> },
+  { suit: "spades", rank: 10, img: <SpadesJ /> },
+  { suit: "spades", rank: 10, img: <SpadesQ /> },
+  { suit: "spades", rank: 10, img: <SpadesK /> },
+
+  { suit: "diamonds", rank: 11, img: <DiamondsAce /> },
+  { suit: "diamonds", rank: 2, img: <Diamonds2 /> },
+  { suit: "diamonds", rank: 3, img: <Diamonds3 /> },
+  { suit: "diamonds", rank: 4, img: <Diamonds4 /> },
+  { suit: "diamonds", rank: 5, img: <Diamonds5 /> },
+  { suit: "diamonds", rank: 6, img: <Diamonds6 /> },
+  { suit: "diamonds", rank: 7, img: <Diamonds7 /> },
+  { suit: "diamonds", rank: 8, img: <Diamonds8 /> },
+  { suit: "diamonds", rank: 9, img: <Diamonds9 /> },
+  { suit: "diamonds", rank: 10, img: <Diamonds10 /> },
+  { suit: "diamonds", rank: 10, img: <DiamondsJ /> },
+  { suit: "diamonds", rank: 10, img: <DiamondsQ /> },
+  { suit: "diamonds", rank: 10, img: <DiamondsK /> },
+
+  { suit: "clubs", rank: 11, img: <ClubsAce /> },
+  { suit: "clubs", rank: 2, img: <Clubs2 /> },
+  { suit: "clubs", rank: 3, img: <Clubs3 /> },
+  { suit: "clubs", rank: 4, img: <Clubs4 /> },
+  { suit: "clubs", rank: 5, img: <Clubs5 /> },
+  { suit: "clubs", rank: 6, img: <Clubs6 /> },
+  { suit: "clubs", rank: 7, img: <Clubs7 /> },
+  { suit: "clubs", rank: 8, img: <Clubs8 /> },
+  { suit: "clubs", rank: 9, img: <Clubs9 /> },
+  { suit: "clubs", rank: 10, img: <Clubs10 /> },
+  { suit: "clubs", rank: 10, img: <ClubsJ /> },
+  { suit: "clubs", rank: 10, img: <ClubsQ /> },
+  { suit: "clubs", rank: 10, img: <ClubsK /> },
+
+];
